2aae6c184b8079c6e8f1f3a7de972bed84f01376
JackBarley <38003173+JackBarley@users.noreply.github.com>
2018-04-03 14:29:27 +0700

    Add files via upload

---
+++ OnlineStore App/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCKOYw8BOOKKRQiWv4xXxuvOfjzqbQH6nM",
+  authDomain: "catch-of-the-day-khiemdo.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-khiemdo.firebaseio.com",
+  projectId: "catch-of-the-day-khiemdo"
+});
+
+export default base;
